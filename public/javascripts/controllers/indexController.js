43dc4af1518cc1aa349dd8cfd2aabf876959c22f
Skynessthedark <qksnn.9@gmail.com>
2020-11-16 20:24:06 +0300

    new user

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,10 +1,22 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) =>{
-    indexFactory.connectSocket('http://localhost:3000',{
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
-    }).then((socket)=>{
-        console.log('Bağlantı sağlandı.', socket);
-    }).catch((err) =>{
-        console.log(err);
-    });
+
+    $scope.init = () =>{
+        const username = prompt("Enter your username: ");
+
+        if(username)
+            initSocket(username);
+        else
+            return false;
+    }
+
+    function initSocket(username){
+        indexFactory.connectSocket('http://localhost:3000',{
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        }).then((socket)=>{
+            socket.emit('newUser', {username});
+        }).catch((err) =>{
+            console.log(err);
+        });
+    }
 }]);
--- src/socketAPI.js
+++ src/socketAPI.js
@@ -2,11 +2,26 @@ const socketio = require('socket.io');
 const io = socketio();
 
 const socketAPI = { };
-
 socketAPI.io = io;
+
+const users = [];
 
 io.on('connection', (socket)=>{
     console.log('a user connected.');
+
+    socket.on('newUser', (data)=>{
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x: 0,
+                y: 0
+            }
+        };
+
+        const userData = Object.assign(data, defaultData);
+        users.push(userData);
+        console.log(users);
+    });
 });
 
 module.exports = socketAPI;
